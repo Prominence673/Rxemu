334612690a199c6643501965cd6e1c6340686369
Lautaro <safarakiri@gmail.com>
2026-07-22 18:10:04 +0000

    feat(pause-command): connect pause command to daemon

--- internal/commands/pause.go
+++ internal/commands/pause.go
@@ -1,20 +1,37 @@
 package commands
 
 import (
+	"fmt"
+
+	"github.com/Prominence673/rxemu/internal/config"
+	"github.com/Prominence673/rxemu/internal/ipc"
 	"github.com/spf13/cobra"
 )
 
+func pause() ipc.Response {
+	req := ipc.Request{Command: "pause"}
+	cfg, err := config.Load()
+	if err != nil {
+		return ipc.Response{OK: false, Error: err.Error()}
+	}
+	client := ipc.NewClient(cfg.SocketPath)
+	res, err := client.Send(req)
+	if err != nil {
+		return ipc.Response{OK: false, Error: err.Error()}
+	}
+	return res
+}
+
 var pauseCmd = &cobra.Command{
-	Use: "pause",
+	Use:   "pause",
 	Short: "pause current music",
-	RunE: func(cmd *cobra.Command, args []string) error{
-		if err := cmd.Help(); err != nil{
-			return err
-		}
+	RunE: func(cmd *cobra.Command, args []string) error {
+		res := pause()
+		fmt.Println(res)
 		return nil
 	},
 }
 
-func init(){
+func init() {
 	rootCmd.AddCommand(pauseCmd)
 }
