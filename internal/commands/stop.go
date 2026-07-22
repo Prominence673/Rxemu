064063cbeb891357f37b43d46185f2ddbd056db2
Lautaro <safarakiri@gmail.com>
2026-07-22 18:10:04 +0000

    feat(stop-command): add stop command

---
+++ internal/commands/stop.go
@@ -0,0 +1,36 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/Prominence673/rxemu/internal/config"
+	"github.com/Prominence673/rxemu/internal/ipc"
+	"github.com/spf13/cobra"
+)
+
+func stop() ipc.Response {
+	req := ipc.Request{Command: "stop"}
+	cfg, err := config.Load()
+	if err != nil {
+		return ipc.Response{OK: false, Error: err.Error()}
+	}
+	client := ipc.NewClient(cfg.SocketPath)
+	res, err := client.Send(req)
+	if err != nil {
+		return ipc.Response{OK: false, Error: err.Error()}
+	}
+	return res
+}
+
+var stopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "stop current music",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		res := stop()
+		fmt.Println(res)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stopCmd)
+}
