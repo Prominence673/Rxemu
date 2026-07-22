a5f0efcd1020822f74aaa52dc651e359401c27a8
Lautaro <safarakiri@gmail.com>
2026-07-22 18:10:04 +0000

    feat(play-command): connect play command to daemon

--- internal/commands/play.go
+++ internal/commands/play.go
@@ -1,20 +1,37 @@
 package commands
 
 import (
+	"fmt"
+	"github.com/Prominence673/rxemu/internal/config"
+	"github.com/Prominence673/rxemu/internal/ipc"
 	"github.com/spf13/cobra"
 )
 
+func play(url string) ipc.Response {
+	req := ipc.Request{Command: "play", Args: []string{url}}
+	cfg, err := config.Load()
+	if err != nil {
+		return ipc.Response{OK: false, Error: err.Error()}
+	}
+	client := ipc.NewClient(cfg.SocketPath)
+	res, err := client.Send(req)
+	if err != nil {
+		return ipc.Response{OK: false, Error: err.Error()}
+	}
+	return res
+}
+
 var playCmd = &cobra.Command{
-	Use: "play",
+	Use:   "play <url>",
 	Short: "play current music",
-	RunE: func(cmd *cobra.Command, args []string) error{
-		if err := cmd.Help(); err != nil{
-			return err
-		}
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		res := play(args[0])
+		fmt.Println(res)
 		return nil
 	},
 }
 
-func init(){
+func init() {
 	rootCmd.AddCommand(playCmd)
 }
