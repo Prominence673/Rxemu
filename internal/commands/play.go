9823d4928e29a79c92c5c68f2b66035658048d31
Lautaro <safarakiri@gmail.com>
2026-07-23 02:21:10 +0000

    feat(play-command): support track selection and URLs

--- internal/commands/play.go
+++ internal/commands/play.go
@@ -7,8 +7,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func play(url string) ipc.Response {
-	req := ipc.Request{Command: "play", Args: []string{url}}
+func play(arg string, url bool) ipc.Response {
+	var req ipc.Request
+	if url{
+		req = ipc.Request{Command: "playurl", Args: []string{arg}}
+	}else{
+		req = ipc.Request{Command: "play", Args: []string{arg}}
+	}
 	cfg, err := config.Load()
 	if err != nil {
 		return ipc.Response{OK: false, Error: err.Error()}
@@ -22,16 +27,26 @@ func play(url string) ipc.Response {
 }
 
 var playCmd = &cobra.Command{
-	Use:   "play <url>",
+	Use:   "play <id>",
 	Short: "play current music",
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		res := play(args[0])
-		fmt.Println(res)
+		var res ipc.Response
+		if v,_ := cmd.Flags().GetBool("url"); v{
+			res = play(args[0], true)
+		} else{
+			res = play(args[0], false)
+		}
+		if res.OK{
+			fmt.Println(res.Message)
+		}else{
+			fmt.Println(res.Error)
+		}
 		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(playCmd)
+	playCmd.Flags().BoolP("url", "u", false, "play <url> -u")
 }
