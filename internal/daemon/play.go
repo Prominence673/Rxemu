f8b7710f589f5792670670912bcc6a0cde667634
Lautaro <safarakiri@gmail.com>
2026-07-23 02:21:10 +0000

    feat(play): play search results and URLs

--- internal/daemon/play.go
+++ internal/daemon/play.go
@@ -1,23 +1,87 @@
 package daemon
 
-import "github.com/Prominence673/rxemu/internal/ipc"
+import (
+	"context"
+	"fmt"
+	"strconv"
 
-func (d *Daemon) play(req ipc.Request) ipc.Response {
+	"github.com/Prominence673/rxemu/internal/commands"
+	"github.com/Prominence673/rxemu/internal/ipc"
+)
+
+func (d *Daemon) play(req ipc.Request) ipc.Response{
 	if len(req.Args) == 0 || req.Args[0] == "" {
 		return ipc.Response{
 			OK:    false,
-			Error: "URL is required",
+			Error: "ID is required",
 		}
 	}
-	if err := d.player.play(req.Args[0]); err != nil {
+	if len(d.LastTrack) == 0{
+		return ipc.Response{
+			OK:    false,
+			Error: "First search a song",
+		}
+	}
+	
+	id, err := strconv.Atoi(req.Args[0])
+	if err != nil{
+		return ipc.Response{
+			OK:    false,
+			Error: "Invalid ID",
+		}
+	}
+	if id < 1 || id > len(d.LastTrack) {
+    return ipc.Response{
+        OK:    false,
+        Error: "track number is out of range",
+    }
+	}
+	if err := d.player.play(d.LastTrack[id-1].URL); err != nil {
 		return ipc.Response{
 			OK:    false,
 			Error: err.Error(),
 		}
 	}
 	d.state.Status = statePlaying
+	d.CurrentTrack = &d.LastTrack[id-1]
+	min := commands.FormatDuration(d.CurrentTrack.Duration)
 	return ipc.Response{
 		OK:      true,
-		Message: string(d.state.Status),
+		Message: fmt.Sprintf("Playing ▶︎ %s - %s - %s", d.CurrentTrack.Title, d.CurrentTrack.Artist, min),
 	}
+} 
+
+func (d *Daemon) playURL(req ipc.Request) ipc.Response {
+    if len(req.Args) == 0 || req.Args[0] == "" {
+        return ipc.Response{
+            OK:    false,
+            Error: "URL is required",
+        }
+    }
+
+    track, err := d.searcher.Resolve(
+        context.Background(),
+        req.Args[0],
+    )
+    if err != nil {
+        return ipc.Response{
+            OK:    false,
+            Error: err.Error(),
+        }
+    }
+
+    if err := d.player.play(track.URL); err != nil {
+        return ipc.Response{
+            OK:    false,
+            Error: err.Error(),
+        }
+    }
+
+    d.CurrentTrack = &track
+    d.state.Status = statePlaying
+    min := commands.FormatDuration(d.CurrentTrack.Duration)
+    return ipc.Response{
+        OK:      true,
+        Message: fmt.Sprintf("Playing ▶︎ %s - %s - %s", d.CurrentTrack.Title, d.CurrentTrack.Artist, min),
+    }
 }
