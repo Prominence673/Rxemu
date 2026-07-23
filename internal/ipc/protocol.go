d3295c9ae133bec64b40dc6089d11c2e09628579
Lautaro <safarakiri@gmail.com>
2026-07-23 02:21:10 +0000

    feat(protocol): return tracks in IPC responses

--- internal/ipc/protocol.go
+++ internal/ipc/protocol.go
@@ -1,12 +1,15 @@
 package ipc
 
+import "github.com/Prominence673/rxemu/internal/source"
+
 type Request struct {
 	Command string   `json:"command"`
-	Args    []string `json:"query,omitempty"`
+	Args    []string `json:"args,omitempty"`
 }
 
 type Response struct {
 	OK      bool   `json:"ok"`
 	Message string `json:"message,omitempty"`
 	Error   string `json:"error,omitempty"`
+	Tracks []source.Track `json:"tracks,omitempty"`
 }
