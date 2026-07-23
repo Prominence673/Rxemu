f3f1b27ea9b9e5074e00500a5e3e23b38346fff8
Lautaro <safarakiri@gmail.com>
2026-07-23 02:21:10 +0000

    feat(daemon): track searches and player events

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -1,6 +1,9 @@
 package daemon
 
-import "github.com/Prominence673/rxemu/internal/ipc"
+import (
+	"github.com/Prominence673/rxemu/internal/ipc"
+	"github.com/Prominence673/rxemu/internal/source"
+)
 
 type PlayStatus string
 
@@ -17,12 +20,29 @@ type State struct {
 type Daemon struct {
 	state  State
 	player *Player
+	searcher source.Searcher
+	CurrentTrack *source.Track
+	LastTrack []source.Track
 }
 
-func New(p *Player) *Daemon {
-	return &Daemon{state: State{Status: stateStopped}, player: p}
+func New(p *Player, s source.Searcher) *Daemon {
+	return &Daemon{
+		state: State{Status: stateStopped}, 
+		player: p,
+		searcher: s,
+	}
 }
+func (d *Daemon) WatchPlayer() {
+	for event := range d.player.Events() {
+		switch event.Event {
+		case "file-loaded":
+			d.state.Status = statePlaying
 
+		case "end-file":
+			d.state.Status = stateStopped
+		}
+	}
+}
 func (d *Daemon) Handle(req ipc.Request) ipc.Response {
 	switch req.Command {
 	case "status":
@@ -31,8 +51,12 @@ func (d *Daemon) Handle(req ipc.Request) ipc.Response {
 		return d.stop()
 	case "play":
 		return d.play(req)
+	case "playurl":
+		return d.playURL(req)
 	case "pause":
 		return d.pause()
+	case "search":
+		return d.search(req)
 	default:
 		return ipc.Response{OK: false, Message: "Invalid command"}
 	}
